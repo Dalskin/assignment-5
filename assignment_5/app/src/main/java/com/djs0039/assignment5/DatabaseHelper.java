539cd0b83b8f095f5a9bc135699bf3463219040d
dalskin <frappacino1197@gmail.com>
2020-04-15 16:55:31 -0500

    final

---
+++ assignment_5/app/src/main/java/com/djs0039/assignment5/DatabaseHelper.java
@@ -0,0 +1,54 @@
+package com.djs0039.assignment5;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "TransactionHistory.db";
+    public static final String TABLE_NAME = "transactionsTable";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "DATE";
+    public static final String COL_3 = "AMOUNT";
+    public static final String COL_4 = "CATEGORY";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "DATE TEXT,AMOUNT TEXT,CATEGORY TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String date, String amount, String category) {
+        ContentValues contentValues = new ContentValues();
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        contentValues.put(COL_2, date);
+        contentValues.put(COL_3, amount);
+        contentValues.put(COL_4, category);
+        long isInsertSuccessful = db.insert(TABLE_NAME, null, contentValues);
+        if (isInsertSuccessful == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return result;
+    }
+}
---
+++ assignment_5/app/src/main/java/com/djs0039/assignment5/MainActivity.java
@@ -0,0 +1,132 @@
+package com.djs0039.assignment5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import java.math.BigDecimal;
+
+public class MainActivity extends AppCompatActivity {
+
+    BigDecimal balance;
+    Button enter_btn;
+    Context applicationContext;
+    DatabaseHelper myDB;
+    EditText date, amount, purpose;
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+    TableLayout tableLayout;
+    TextView header;
+
+    private View.OnClickListener listen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            enter_btn_clicked();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDB = new DatabaseHelper(this);
+        header = findViewById(R.id.header);
+        date = findViewById(R.id.date);
+        amount = findViewById(R.id.amount);
+        purpose = findViewById(R.id.purpose);
+        enter_btn = findViewById(R.id.enter_btn);
+        tableLayout = findViewById(R.id.transactionsTable);
+        applicationContext = this;
+
+        sharedPref = getSharedPreferences(
+                "com.djs0039.assignment5.PREFERENCE_FILE_KEY", MODE_PRIVATE);
+
+        enter_btn.setOnClickListener(listen);
+
+        balance = new BigDecimal(sharedPref.getString("BALANCE", "0"));
+        header.setText(sharedPref.getString("HEADER", "Current Balance: $0"));
+        populateTable();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sharedPref = getSharedPreferences(
+                "com.djs0039.assignment5.PREFERENCE_FILE_KEY", MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+        editor.putString("HEADER", header.getText().toString());
+        String balance = header.getText().toString().substring(18);
+        editor.putString("BALANCE", balance);
+        editor.apply();
+    }
+
+    private void enter_btn_clicked() {
+        String dateText = date.getText().toString();
+        String amountText = amount.getText().toString();
+        String purposeText = purpose.getText().toString();
+
+        BigDecimal newBalance = new BigDecimal(amountText);
+
+        balance = balance.add(newBalance);
+        header.setText("Current Balance: $" + balance);
+
+        myDB.insertData(dateText, amountText, purposeText);
+
+        addRow(dateText, amountText, purposeText);
+    }
+
+    private void addRow(String date, String amount, String category) {
+        LinearLayout.LayoutParams tableRowParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TableRow tableRow = new TableRow(this);
+        tableRow.setLayoutParams(tableRowParams);
+
+        TextView dateText = new TextView(this);
+        dateText.setText(date);
+        TextView amountText = new TextView(this);
+        amountText.setText(amount);
+        TextView categoryText = new TextView(this);
+        categoryText.setText(category);
+
+        TableRow.LayoutParams cellParams = new TableRow.LayoutParams(0,
+                TableRow.LayoutParams.MATCH_PARENT);
+        cellParams.weight = 3;
+        dateText.setLayoutParams(cellParams);
+        amountText.setLayoutParams(cellParams);
+        categoryText.setLayoutParams(cellParams);
+
+        tableRow.addView(dateText);
+        tableRow.addView(amountText);
+        tableRow.addView(categoryText);
+
+        tableLayout.addView(tableRow);
+    }
+
+    private void populateTable() {
+        Cursor result = myDB.getAllData();
+        if (result.getCount() == 0) {
+            return;
+        }
+        while (result.moveToNext()) {
+            String date = result.getString(1);
+            String amount = result.getString(2);
+            String category = result.getString(3);
+
+            addRow(date, amount, category);
+        }
+    }
+}
